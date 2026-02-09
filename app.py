c2af2865f1c7917e9e98c02fef1ca840c793cb01
tejdeeptn7 <tejdeeptn8@gmail.com>
2026-02-09 14:00:51 +0530

    -

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def get_db():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="spdjmr@123",
+        password="YOUR_SQL_PASSWORD",
         database="iam_rbac"
     )
 
